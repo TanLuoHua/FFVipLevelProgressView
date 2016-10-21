d43f411072edab60526cc10a7f42e36956ccc1c9
Mr.Yao <yaoquanfeng@goumin.com>
2016-10-21 13:46:13 +0800

    create protect

---
+++ Pods/Target Support Files/Pods-FFVipLevelProgressView/Pods-FFVipLevelProgressView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FFVipLevelProgressView : NSObject
+@end
+@implementation PodsDummy_Pods_FFVipLevelProgressView
+@end
---
+++ Pods/Target Support Files/Pods-FFVipLevelProgressViewTests/Pods-FFVipLevelProgressViewTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FFVipLevelProgressViewTests : NSObject
+@end
+@implementation PodsDummy_Pods_FFVipLevelProgressViewTests
+@end
